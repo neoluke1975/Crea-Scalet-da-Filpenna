920d69e17a3045e80cf24233b10ab95936969bf3
Gianluca Troncone <gtroncone@treelog.it>
2016-11-01 18:20:17 +0100

    Aggiungere i file di progetto.

---
+++ Crea Scalet da Filpenna/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Crea_Scalet_da_Filpenna
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
